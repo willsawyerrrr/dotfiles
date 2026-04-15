ea7c23df0bf76deeddbc578f8199b601004fb431
William Sawyer <williamsawyer@heidihealth.com>
2026-04-15 17:05:14 +1000

    chore(nvim): Consolidate Kitty configer reloader with others

--- dot-config/nvim/lua/config/autocmds/config-reloaders.lua
+++ dot-config/nvim/lua/config/autocmds/config-reloaders.lua
@@ -3,3 +3,9 @@ vim.api.nvim_create_autocmd('BufWritePost', {
   pattern = vim.uv.fs_realpath(vim.fn.expand '~' .. '/.config/aerospace/aerospace.toml'),
   command = ':silent !aerospace reload-config',
 })
+
+vim.api.nvim_create_autocmd('BufWritePost', {
+  desc = 'Auto-reload Kitty config',
+  pattern = vim.fn.expand '~' .. '/.config/kitty/kitty.conf',
+  command = ':silent !kill -SIGUSR1 $KITTY_PID',
+})
