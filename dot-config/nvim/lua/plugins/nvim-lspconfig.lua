9006430f3a6b020cc79e153103c764717fa237d4
William Sawyer <williamsawyer@heidihealth.com>
2026-04-30 09:52:33 +1000

    chore(nvim): Treat `stylua` like normal LSPs

--- dot-config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dot-config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -213,6 +213,8 @@ return {
 
       ruff = {},
 
+      stylua = {},
+
       terraformls = {},
 
       ts_ls = {},
@@ -221,9 +223,7 @@ return {
     }
 
     local ensure_installed = vim.tbl_keys(servers or {})
-    vim.list_extend(ensure_installed, {
-      'stylua', -- Used to format Lua code
-    })
+
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
     require('mason-lspconfig').setup {
