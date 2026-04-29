4131c1951987522779d5c3317e1fde6715c881f4
William Sawyer <williamsawyer@heidihealth.com>
2026-04-30 09:29:53 +1000

    feat(nvim): Keymap for redundant import aliases in Python files

--- dot-config/nvim/after/ftplugin/python.lua
+++ dot-config/nvim/after/ftplugin/python.lua
@@ -1,1 +1,4 @@
 vim.cmd 'iabbrev <buffer> improt import'
+
+-- Yank Inner Word, go to End of word, Append " as ", Esc to normal mode, Paste
+vim.keymap.set('n', '<leader>ia', 'yiwea as <Esc>p', { buffer = true, desc = 'Append redundant alias for word under cursor' })
