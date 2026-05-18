71b67e3d66e9c3668ca5c35d7c04c99c7c296393
William Sawyer <williamsawyer@heidihealth.com>
2026-05-19 04:34:29 +1000

    feat(nvim): Make function and class textobject selection linewise

--- dot-config/nvim/lua/plugins/mini.lua
+++ dot-config/nvim/lua/plugins/mini.lua
@@ -4,17 +4,32 @@ return {
   -- Collection of various small independent plugins/modules
   'nvim-mini/mini.nvim',
   config = function()
+    local function linewise(spec)
+      return function(ai_type, id, opts)
+        local result = spec(ai_type, id, opts)
+        if not result then
+          return
+        end
+        local regions = vim.islist(result) and result or { result }
+        for _, r in ipairs(regions) do
+          r.vis_mode = 'V'
+        end
+        return result
+      end
+    end
+
     -- Better Around/Inside textobjects
     --
     -- Examples:
     --  - va)  - [V]isually select [A]round [)]paren
     --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
     --  - ci'  - [C]hange [I]nside [']quote
+    local ts = require('mini.ai').gen_spec.treesitter
     require('mini.ai').setup {
       n_lines = 500,
       custom_textobjects = {
-        c = require('mini.ai').gen_spec.treesitter { a = '@class.outer', i = '@class.inner' },
-        f = require('mini.ai').gen_spec.treesitter { a = '@function.outer', i = '@function.inner' },
+        c = linewise(ts { a = '@class.outer', i = '@class.inner' }),
+        f = linewise(ts { a = '@function.outer', i = '@function.inner' }),
       },
     }
 
