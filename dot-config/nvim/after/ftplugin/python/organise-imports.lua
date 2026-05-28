fac3a9f73844a0d5344e1e615c81e86a8d391cdb
William Sawyer <williamsawyer@heidihealth.com>
2026-05-29 04:26:22 +1000

    chore(nvim): Split ftplugins

--- dot-config/nvim/after/ftplugin/python/organise-imports.lua
+++ dot-config/nvim/after/ftplugin/python/organise-imports.lua
@@ -1,8 +1,3 @@
-vim.cmd 'iabbrev <buffer> improt import'
-
--- Yank Inner Word, go to End of word, Append " as ", Esc to normal mode, Paste
-vim.keymap.set('n', '<leader>ia', 'yiwea as <Esc>p', { buffer = true, desc = 'Append redundant alias for word under cursor' })
-
 -- Organise imports using Ruff on write
 vim.api.nvim_create_autocmd('BufWritePost', {
   callback = function()
---
+++ dot-config/nvim/after/ftplugin/python/redundant-import-alias.lua
@@ -0,0 +1,2 @@
+-- Yank Inner Word, go to End of word, Append " as ", Esc to normal mode, Paste
+vim.keymap.set('n', '<leader>ia', 'yiwea as <Esc>p', { buffer = true, desc = 'Append redundant alias for word under cursor' })
