5d488eb772ae5717a6cb28aa5fa95caef9b638e8
William Sawyer <williamsawyer@heidihealth.com>
2026-05-31 17:20:31 +1000

    chore(nvim): Autoload autocmds

--- dot-config/nvim/lua/config/autocmds/init.lua
+++ dot-config/nvim/lua/config/autocmds/init.lua
@@ -1,6 +1,9 @@
--- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
-require 'config.autocmds.config-reloaders'
-require 'config.autocmds.kitty-tab-title'
-require 'config.autocmds.highlight-on-yank'
+for _, path in ipairs(vim.fn.glob(vim.fn.stdpath 'config' .. '/lua/config/autocmds/*.lua', false, true)) do
+  local name = vim.fn.fnamemodify(path, ':t:r')
+
+  if name ~= 'init' then
+    require('config.autocmds.' .. name)
+  end
+end
