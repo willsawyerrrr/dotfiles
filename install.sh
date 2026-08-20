344935299a0ac2846ed6423921f7572b9fd818b2
William Sawyer <wmsawyer2609@gmail.com>
2026-08-20 13:50:33 +1000

    feat(brew, install): Install homebrew after oh my zsh

--- install.sh
+++ install.sh
@@ -6,14 +6,14 @@ fi
 
 cd ~/dotfiles/
 
-if ! ( which brew >/dev/null ); then
-    NONINTERACTIVE=1 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
-
 source ./dot-zshenv
 
 if [[ ! -d $ZSH ]]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+if ! ( which brew >/dev/null ); then
+    NONINTERACTIVE=1 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 brew bundle install --file ./dot-config/homebrew/Brewfile
